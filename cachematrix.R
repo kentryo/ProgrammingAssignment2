d920e6f202a3a7fb9eaf879e928611be037e248e
Kentryo <kenhuang@udel.edu>
2015-07-21 15:41:08 -0400

    adjust from mean script

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinvert <- function(invert) m <<- invert
+  getinvert <- function() m
+  list(set = set, get = get, setinvert = setinvert, getinvert = getinvert)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinvert()
+  if(!is.null(m)){
+    message("getting cacahed data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinvert(m)
+  return(m)
+  ## Return a matrix that is the inverse of 'x'
 }
